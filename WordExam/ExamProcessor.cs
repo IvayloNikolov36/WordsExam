7d4bb724e7840b43998cd7fb71e53c9a2074ea22
DESKTOP-3E10FSC\INikolov <ivailonikolov36@gmail.com>
2025-09-19 22:52:10 +0300

    the new Random method from .net 8 is used

--- WordExam/ExamProcessor.cs
+++ WordExam/ExamProcessor.cs
@@ -3,13 +3,13 @@ using EnglishWordsExam.Models;
 using EnglishWordsExam.Strategies;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using static EnglishWordsExam.Constants;
 
 namespace EnglishWordsExam
 {
     public class ExamProcessor
     {
-        private readonly IList<DictionaryWord> words;
+        private readonly DictionaryWord[] words;
         private readonly int wordsToTranslate;
         private readonly TranslationType translationType;
         private readonly IExamStrategy examStrategy;
@@ -20,7 +20,7 @@ namespace EnglishWordsExam
             TranslationType translationType,
             IExamStrategy examStrategy)
         {
-            this.words = words.ToList();
+            this.words = [..words];
             this.wordsToTranslate = wordsToTranslate;
             this.translationType = translationType;
             this.examStrategy = examStrategy;
@@ -28,32 +28,13 @@ namespace EnglishWordsExam
 
         public void Start()
         {
-            Console.WriteLine($"Input {Constants.HintCommand}/{Constants.HintCommandCyrilic} for a help.");
+            Console.WriteLine($"Input {HintCommand}/{HintCommandCyrilic} for a help.");
 
-            IEnumerable<DictionaryWord> examWords = this.GetWordsRandomly();
+            IEnumerable<DictionaryWord> examWords = Random
+                .Shared
+                .GetItems(this.words, this.wordsToTranslate);
 
-            examStrategy.ProcessExam(examWords, this.translationType);
-        }
-
-        private IEnumerable<DictionaryWord> GetWordsRandomly()
-        {
-            Random randomGenerator = new();
-            DictionaryWord[] examWords = new DictionaryWord[this.wordsToTranslate];
-            HashSet<int> usedRandomCrearedIndexes = new();
-
-            for (int i = 0; i < wordsToTranslate; i++)
-            {
-                int randomIndex = randomGenerator.Next(0, this.words.Count);
-                while (usedRandomCrearedIndexes.Contains(randomIndex))
-                {
-                    randomIndex = randomGenerator.Next(0, this.words.Count);
-                }
-
-                usedRandomCrearedIndexes.Add(randomIndex);
-                examWords[i] = this.words[randomIndex];
-            }
-
-            return examWords;
+            this.examStrategy.ProcessExam(examWords, this.translationType);
         }
     }
 }
